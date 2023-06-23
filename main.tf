e791329b61f8b864f109714822b1ceb9856ab46a
Alejandro Ayala Ati <mayalaat@pichincha.com>
2023-06-22 23:08:50 -0500

    Adding bucket policy

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
+locals {
+  whitelist_cidr = ["0.0.0.0/0"]
+}
+
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 
   tags = {
     Environment = "Dev"
-    Project = "Terraform course"
+    Project     = "Terraform course"
   }
 }
 
@@ -13,4 +17,26 @@ resource "aws_s3_object" "object" {
   source = "hello.html"
 
   content_type = "text/html"
+}
+
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.main.json
+}
+
+data "aws_iam_policy_document" "main" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.bucket.arn}/*"]
+    condition {
+      test     = "IpAddress"
+      values   = local.whitelist_cidr
+      variable = "aws:sourceIp"
+    }
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+  }
 }
